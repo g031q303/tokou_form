a4af9a9a166d3f0d45b2c2dcd4018281de178f0c
g031q303 <67403243+g031q303@users.noreply.github.com>
2020-07-01 22:08:53 +0900

    Add files via upload

---
+++ db_check.php
@@ -0,0 +1,11 @@
+<?php
+// 管理ページのログインパスワード
+define( 'PASSWORD', 'adminPassword');
+
+// データベースの接続情報
+define( 'DB_HOST', 'localhost');
+define( 'DB_USER', 'g031q303');
+define( 'DB_PASS', 'j2vcPFbyqE');
+define( 'DB_NAME', 'g031q303');
+
+?>
